d21a34a72ed1ff64082c936fd9ff78a6fb9660cb
braum852 <julian.teo59@yahoo.com>
2022-02-12 19:38:00 +1000

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    real_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def real_triangle
+    shape = [(a + b > c), (b + c > a), (a + c > b)]
+    [a, b, c].each do |side|
+      shape << false if side <= 0 
+    raise TriangleError if shape.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
